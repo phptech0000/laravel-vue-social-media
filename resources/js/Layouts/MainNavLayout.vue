27b0e03b521b875df239c467624807387797f188
phptech0000 <php.tech0000@outlook.com>
2023-06-02 22:34:29 +0300

    ft: create image cropper modal

--- resources/js/Layouts/MainNavLayout.vue
+++ resources/js/Layouts/MainNavLayout.vue
@@ -14,6 +14,8 @@ import FacebookMessenger from "vue-material-design-icons/FacebookMessenger.vue";
 import Bell from "vue-material-design-icons/Bell.vue";
 import Logout from "vue-material-design-icons/Logout.vue";
 
+import CropperModal from "@/Components/CropperModal.vue";
+
 // Pinia
 import { useGeneralStore } from "@/stores/general";
 import { storeToRefs } from "pinia";
@@ -179,4 +181,6 @@ let showMenu = ref(false);
     </div>
   </div>
   <slot />
+
+  <CropperModal v-if="isCropperModal" @showModal="isCropperModal = false" />
 </template>
